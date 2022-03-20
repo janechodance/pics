4cdbfd118c6693dc7c6494e94f0ad665b5e0f22c
Jane <janechodance@gmail.com>
2022-03-20 19:26:08 -0400

    controlled form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = { term: ''};
+    // onInputChange(event){
+    //   this.setState({term: event.target.value})
+    //   console.log(this.state.term)
+    // }
     render(){
         return(
             <div className="ui segment">
             <form className='ui form'>
                 <div className='field'>
                 <label>Image Search</label>
-                <input type="text"/>
+                <input type="text" value ={this.state.term} onChange={(e)=> this.setState({term: e.target.value})}/>
                 </div>
                 
             </form>
