90c190fbf2c7c9b28ab970fe81e9392a1c0f4f36
Jane <janechodance@gmail.com>
2022-03-20 20:09:30 -0400

    submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 class SearchBar extends React.Component{
     state = { term: ''};
-    // onInputChange(event){
-    //   this.setState({term: event.target.value})
-    //   console.log(this.state.term)
-    // }
+    onInputChange=(event)=>{
+      this.setState({term: event.target.value})
+      console.log(this.state.term)
+    }
+    // use arrow function to bind "this" to the instance of the class so won't have undefined error
+    onFormSubmit = (event)=>{
+        event.preventDefault();
+        console.log(this.state.term)
+        this.props.getTerm(this.state.term)
+    }
     render(){
         return(
             <div className="ui segment">
-            <form className='ui form'>
+            <form className='ui form' onSubmit={this.onFormSubmit}>
                 <div className='field'>
                 <label>Image Search</label>
-                <input type="text" value ={this.state.term} onChange={(e)=> this.setState({term: e.target.value})}/>
+                <input type="text" value ={this.state.term} onChange={this.onInputChange}/>
                 </div>
                 
             </form>
