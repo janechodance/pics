83e0442752463dabf5a060e756d9dd378c4c6f1a
Jane <janechodance@gmail.com>
2022-03-21 14:08:40 -0400

    styling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 class ImageCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+        this.state = { spans: 0}
+    }
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load', this.setSpans);
+        
+    }
+    setSpans = ()=> {
+        console.log(this.imageRef.current.height)
+        const height = this.imageRef.current.height
+        const spans = Math.ceil(height/10);
+        this.setState({spans})
+        }
     render(){
+        const {description, urls} = this.props.image
         return(
-            <div>
-                <img alt={this.props.image.description}
-                src={this.props.image.urls.regular}/>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+                <img ref={this.imageRef} alt={description} src={urls.regular}/>
             </div>
         )
     }
